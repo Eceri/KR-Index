c85f85b8c6662c51fd4ec213578f051b30195016
Michael Lorens <michael.lorens@hs-furtwangen.de>
2019-11-18 13:39:44 +0100

    fixed: still etc errors

--- Client/src/Etc.js
+++ Client/src/Etc.js
@@ -10,8 +10,4 @@ export class Etc extends Component {
         );
     }
 }
-<<<<<<< HEAD:Client/src/etc.js
-export default Etc
-=======
 export default Etc;
->>>>>>> 2b7a194b79fa5f7d927d9c0e78550e7ead290d1c:Client/src/Etc.js
