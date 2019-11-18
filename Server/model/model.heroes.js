3b923a4ddaa1173373088919e9fa4591f80b03ec
eceri <mleceri@gmail.com>
2019-11-18 04:11:30 +0100

    feat/chore: added first iteration of the heroes.schema and a rip of all hero data in preparation for the DB

---
+++ Server/model/model.heroes.js
@@ -0,0 +1,97 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const positions = [
+  "Front",
+  "Middle",
+  "Back"
+]
+const gender = [
+  "male",
+  "female",
+  "???"
+]
+const DataSchema = new Schema(
+  { //TODO: go through heroes.json and see what can be added were in the Schema!
+    id: Number,
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    title: String,
+    class: String,
+    position: positions,
+    weapon: {
+      uniqueWeapon: {
+        name: String,
+        effect: [String]
+      },
+      soulWeapon: {
+        advancement0: String,
+        advancement1: String,
+        advancement2: String,
+        activation: String,
+        cd: Number,
+        charges: Number
+      }
+    },
+    skills: [
+      {
+        skillId: Number,
+        skillInfo: {
+          skillNumber: String,
+          name: String,
+          effect: String,
+          cd: Number,
+          cost: Number
+        },
+        linked: Boolean,
+        light: String,
+        dark: String,
+        books: [String],
+        uniqueTreasure: {
+          name: String,
+          effect: [String],
+          introduction: String
+        }
+      }
+    ],
+    light: String,
+    dark: String,
+    skins: [String],
+    background: {
+      story: String,
+      profile: { //TODO: look for abnormalities in the profile points ingame
+        gender: genders,
+        race: String,
+        age: Number,
+        height: Number,
+        birthday: String,
+        constellation: String, //TODO: look up if you could use an ENUM here
+        likes: String,
+        dislikes: String
+      },
+      itemStories: {
+        uniqueWeapon: String, 
+        uniqueTreasure: [String]
+      },
+      voice: {
+        actor: {
+          jp: String,
+          kr: String,
+          en: String
+        },
+        lines: {
+          jp: [String],
+          kr: [String],
+          en: [String]
+        }
+      }
+    }
+  },
+  { timestamps: true, collection: "heroes" }
+);
+
+export default mongoose.model("Heroes", DataSchema);
