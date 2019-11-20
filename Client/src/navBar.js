da0e690b1ad48c05d7cee4a8c44f6b067babab13
eceri <mleceri@gmail.com>
2019-11-20 20:48:26 +0100

    refactoring: some changes to the CSS and navbar

--- Client/src/navBar.js
+++ Client/src/navBar.js
@@ -1,43 +1,23 @@
-import React, { Component } from "react";
-import { Route, Link, Switch } from "react-router-dom";
-import Hero from "./Components/Hero/Hero";
-import Artifacts from "./Components/Artifacts/Artifacts";
-import Etc from "./Etc";
-import { HeroesMenu, Heroes } from "./Components/components";
-import { Maya } from "./Components/components";
-class NavBar extends Component {
-  render() {
-    return (
-      <div>
-        <nav id={"nav"}>
-          <Link to={"/"} className={"navLink"}>
-            <img
-              src={require("./Assets/iconTest.png")}
-              alt={"nagatoro.jpg"}
-              style={{ width: 24, border: "none" }}
-            />
-          </Link>
-          <HeroesMenu className={"navLink"} />
-          <Link to={"/artifacts"} className={"navLink"}>
-            Artifacts
-          </Link>
-          <Link to={"/etc"} className={"navLink"}>
-            Etc.
-          </Link>
-          <Link to={"/heroes"} className="navLink">
-          Heroes(Temp)</Link>
-        </nav>
-        <Switch>
-          <Route push={true} path="/hero/:hero" component={Hero} />
-          <Route path="/artifact/:artifact" component={Artifacts} />
-          <Route path="/artifacts" component={Artifacts} />
-          <Route path="/etc" component={Etc} />
-          <Route path="/Maya" component={Maya} />
-          <Route path="/Heroes" component={Heroes} />
-        </Switch>
-      </div>
-    );
-  }
-}
+import React from "react";
+import { NavLink } from "react-router-dom";
 
+export const NavBar = () => (
+  <nav id={"nav"}>
+    <NavLink to={"/"} className={"navLink"}>
+      <img
+        src={require("./Assets/iconTest.png")}
+        alt={"nagatoro.jpg"}
+        style={{ width: 24, border: "none" }}
+      />
+    </NavLink>
+    <NavLink to={"/heroes"} className="navLink">
+      Heroes</NavLink>
+    <NavLink to={"/artifacts"} className={"navLink"}>
+      Artifacts
+      </NavLink>
+    <NavLink to={"/etc"} className={"navLink"}>
+      Etc.
+          </NavLink>
+  </nav>
+)
 export default NavBar;
