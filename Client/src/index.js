4a8d05edf737b4b2023c0d9e51df30e1dd823698
eceri <mleceri@gmail.com>
2019-11-22 21:42:23 +0100

    feat: added react-snap for a trial run

--- Client/src/index.js
+++ Client/src/index.js
@@ -5,24 +5,12 @@ import "./Components/styles/base.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Hero, Heroes, Maya, Artifacts, Etc } from "./Components/components";
 import Home from "./home";
+import { hydrate, render } from "react-dom";
+import App from "./App"
 
-ReactDOM.render(
-  <div id="pageContainer">
-    <BrowserRouter>
-      <NavBar key={"components.js"} />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/Heroes" component={Heroes} />
-        <Route push={true} path="/heroes/:hero" component={Hero} />
-        <Route path="/artifact/:artifact" component={Artifacts} />
-        <Route path="/artifacts" component={Artifacts} />
-        <Route path="/etc" component={Etc} />
-        <Route path="/Maya" component={Maya} />
-      </Switch>
-      <footer id={"footer"}>
-        <p>Everything is under construction.</p>
-      </footer>
-    </BrowserRouter>
-  </div>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
