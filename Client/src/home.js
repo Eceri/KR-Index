0ebf29f7aab698838617b3e3a30c34b2a5f4a822
eceri <mleceri@gmail.com>
2019-12-01 20:09:47 +0100

    chore: add skill icon 1, 4, 5

--- Client/src/home.js
+++ Client/src/home.js
@@ -4,12 +4,12 @@ export const Home = () => {
   return (
     <React.Fragment>
       <div>
-        <h1 style={{textAlign: "center"}}>Welcome to KRC</h1>
+        <h1 style={{textAlign: "center"}}>Welcome to KR Index</h1>
         <p></p>
         <div>
           Currently working on:
           <ul>
-            <li>Adding hero data to the DB</li>
+            <li>Gathering all hero icons and skins</li>
             <li>Artifacts?</li>
           </ul>
         </div>
