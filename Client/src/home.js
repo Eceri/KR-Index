a20633572b3774981187ca3c5b007935d0a08cd7
eceri <mleceri@gmail.com>
2019-12-10 02:10:48 +0100

    feat: add SW info

--- Client/src/home.js
+++ Client/src/home.js
@@ -7,7 +7,7 @@ export const Home = () => {
         <h1 style={{textAlign: "center"}}>Welcome to KR Index</h1>
         <p>This is a WIP. We're missing a bunch of info, but we are working on it!</p>
         <p>
-          If you have any suggestions or found errors, you can leave us a ticket over at  <a style={{color: "lightblue"}} href="https://github.com/Eceri/KR-Index" target="_blank" rel="noopener noreferrer">GitHub</a> message me on Discord (Eceri#2547) or <a style={{color: "lightblue"}} href="http://www.reddit.com/u/-Eceri" target="_blank" rel="noopener noreferrer">reddit</a>.
+          If you have any suggestions or found errors, you can leave us a ticket over at  <a style={{color: "lightblue"}} href="https://github.com/Eceri/KR-Index" target="_blank" rel="noopener noreferrer">GitHub</a>, message me on Discord (Eceri#2547) or <a style={{color: "lightblue"}} href="http://www.reddit.com/u/-Eceri" target="_blank" rel="noopener noreferrer">reddit</a>.
         </p>
       </div>
     </React.Fragment>
