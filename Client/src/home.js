0dcdce0d20bcbe7bf3e1686f1c1480242f08c8ab
eceri <mleceri@gmail.com>
2019-12-14 12:06:07 +0100

    fix: remove tabs in hero for missing info(skins, voice)

--- Client/src/home.js
+++ Client/src/home.js
@@ -18,7 +18,7 @@ export const Home = () => {
           If you have any suggestions or found errors, you can leave us a ticket over at  <BlueLink  href="https://github.com/Eceri/KR-Index" target="_blank" rel="noopener noreferrer">GitHub</BlueLink>.
         </p>
         <p style={{marginTop: "40px"}}>
-          Currently we are trying to finish up heroes with their Soul Weapons, skins, voicelines and stories. All <Link to="heroes"><BlueLink>heroes</BlueLink></Link> with their skill and UT/UW data and <Link to="artifacts"><BlueLink>artifacts</BlueLink></Link> are available.
+          Currently we are trying to finish up heroes with their Soul Weapons and stories. After that we'll add skins and voicelines. All <Link to="heroes"><BlueLink>heroes</BlueLink></Link> with their skill and UT/UW data and <Link to="artifacts"><BlueLink>artifacts</BlueLink></Link> are already available.
         </p>
       </div>
     </React.Fragment>
