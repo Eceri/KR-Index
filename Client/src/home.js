03e1ed138cce7ebf756c4736e9734d17e0c6c4f1
eceri <mleceri@gmail.com>
2020-01-02 19:22:04 +0100

    feat: change home message

--- Client/src/home.js
+++ Client/src/home.js
@@ -12,9 +12,6 @@ export const Home = () => {
         <p>
           If you have any suggestions or found errors, you can leave us a ticket over at  <Link href="https://github.com/Eceri/KR-Index" target="_blank" style={{color: "lightblue"}}>GitHub</Link>.
         </p>
-        <p style={{marginTop: "40px"}}>
-          Currently we are trying to finish up heroes with their Soul Weapons and stories. After that we'll add skins and voicelines. All <Link to="heroes" style={{color: "lightblue"}} >heroes</Link> with their skill and UT/UW data and <Link to="artifacts" style={{color: "lightblue"}} >artifacts</Link> are already available.
-        </p>
       </div>
     </React.Fragment>
   );
