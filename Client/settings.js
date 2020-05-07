18be3132b2db7dfee094536bd8d6b6dde1e3c19f
LLath <rothpatrick12@yahoo.de>
2020-05-07 05:08:28 +0200

    test: aws serverless function

--- Client/settings.js
+++ Client/settings.js
@@ -1,7 +1,7 @@
 export const settings = () => ({
   api:
     process.env.NODE_ENV === "production"
-      ? "https://ropatr.com/api/"
+      ? "https://oozhepz1vj.execute-api.us-east-1.amazonaws.com/dev/"
       : "http://localhost:8080/api/",
   socket:
     process.env.NODE_ENV === "production"
