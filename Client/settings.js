ff65dec0bdb0a45b5e27d5d212ab37a0ba0af30a
LLath <rothpatrick12@yahoo.de>
2020-05-13 14:58:47 +0200

    fix: variables

--- Client/settings.js
+++ Client/settings.js
@@ -1,8 +1,6 @@
 export const settings = () => ({
   api:
-    process.env.NODE_ENV === "production"
-      ? "https://oozhepz1vj.execute-api.us-east-1.amazonaws.com/dev/"
-      : "http://localhost:8080/api/",
+    process.env.NODE_ENV === "production" ? "" : "http://localhost:8080/api/",
   socket:
     process.env.NODE_ENV === "production"
       ? "https://ropatr.com"
