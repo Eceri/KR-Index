2d86f1221eee9d7030e2cd3a4efd1be3bff41162
LLath <rothpatrick12@yahoo.de>
2020-06-03 11:05:57 +0200

    style: news wip styling

--- src/helpers/aws/aws.helpers.js
+++ src/helpers/aws/aws.helpers.js
@@ -129,7 +129,7 @@ export const getHeroSkins = `query GetHeroSkins($name: String!){
 }`;
 
 export const listPlugPosts = `query ListPlugPosts($nextToken: String!){
-  listPlugGames(filter: {timestamp: {gt: 24}} nextToken: $nextToken){
+  listPlugGames(filter: {timestamp: {gt: 26}} nextToken: $nextToken){
     items {
       timestamp
       thumbnail
