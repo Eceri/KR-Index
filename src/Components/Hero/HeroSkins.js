ccc893780a024ab19c49fd96c7743fd3863db364
eceri <michael.lorens95@gmail.com>
2020-09-04 02:40:35 +0200

    wip: add image gallery to skins

--- src/Components/Hero/HeroSkins.js
+++ src/Components/Hero/HeroSkins.js
@@ -1,38 +1,50 @@
 import React, { useState, useEffect, getGlobal } from "reactn";
-import "../styles/HeroSkins.css";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-
+// import "../styles/HeroSkins.css";
+// import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import "../styles/ImageGallery.css";
 //Relative Imports
 import { AWSoperation, getHeroSkins } from "Helpers";
 
 export const HeroSkins = () => {
-  const [heroSkins, setHeroSkins] = useState(["Base"]);
   const heroName = getGlobal().heroName;
   const assetsUrl = `/assets/heroes/${heroName.toLowerCase()}/`;
+  const getImageObject = (imageSrc) => ({
+    original: `${assetsUrl}${imageSrc}.png`,
+    thumbnail: `${assetsUrl}${imageSrc}.png`,
+    description: imageSrc,
+    thumbnailClass: "custom-thumbnail",
+    originalClass: "custom-image",
+  });
+  const defaultSkins = [getImageObject("Base")];
+  const [heroSkins, setHeroSkins] = useState(defaultSkins);
 
   useEffect(() => {
-    AWSoperation(getHeroSkins, { name: heroName }).then((res) => {
-      setHeroSkins(["Base"].concat(res.data.getHero.skins));
-    });
+    if (heroName != "") {
+      let skins = defaultSkins;
+      AWSoperation(getHeroSkins, { name: heroName }).then((res) => {
+        for (let skin of res.data.getHero.skins) {
+          skins.push(getImageObject(skin));
+        }
+        setHeroSkins(skins);
+      });
+    }
   }, [heroName]);
-  
+
   return (
-    <Tabs>
-      <TabList style={{ border: "none", flexWrap: "wrap" }}>
-        {heroSkins.map((skin) => (
-          <Tab key={`Tab-${skin}`}>{skin}</Tab>
-        ))}
-      </TabList>
-      {heroSkins.map((skin) => (
-        <TabPanel key={`TabPanel-${skin}`}>
-          <img
-            src={`${assetsUrl}${skin}.png`}
-            alt={name}
-            style={{ border: "none" }}
-            className="skin"
-          />
-        </TabPanel>
-      ))}
-    </Tabs>
+    <ImageGallery
+      items={heroSkins}
+      showPlayButton={false}
+      thumbnailPosition={
+        window.matchMedia("(max-width:650px)").matches ? "bottom" : "left"
+      }
+      showFullscreenButton={false}
+      infinite={true}
+      showIndex={false}
+      disableThumbnailScroll={true}
+      showBullets={true}
+      slideOnThumbnailOver={false}
+    />
   );
 };
