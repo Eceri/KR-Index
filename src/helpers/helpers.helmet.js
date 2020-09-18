8787f5ed91dbe7e02b4d6f581ba3d108d06ee484
LLath <rothpatrick12@yahoo.de>
2020-09-18 19:18:54 +0200

    chore: make seo work

--- src/helpers/helpers.helmet.js
+++ src/helpers/helpers.helmet.js
@@ -7,20 +7,34 @@ import Helmet from "react-helmet";
  * @param {*} content
  * @param {*} image
  */
-export const createHelmet = (title, content, image = "") => (
+export const createHelmet = (
+  title = "King's Raid Index|Database",
+  content = "News, Heroes, Artifacts and everything you need to know about King's Raid on one Page",
+  image = "/assets/icons/favicon.png"
+) => (
   <Helmet>
     <title>{title}</title>
+    {/* HTML Meta Tags */}
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
     <meta name="description" content={content} />
-    <link type="application/json+oembed" href="../oembed.json" />
+
+    {/* Google / Search Engine Tags */}
+    <meta itemProp="name" content={title} />
+    <meta itemProp="description" content={content} />
+    <meta itemProp="image" content={`https://dev.krindex.net${image}`} />
+
+    {/* Facebook Meta Tags */}
     <meta property="og:title" content={title} />
     <meta property="og:description" content={content} />
-    <meta
-      property="og:image"
-      content={`https://dev.krindex.net/assets/${image}.png`}
-    />
+    <meta property="og:image" content={`https://dev.krindex.net${image}`} />
     <meta property="og:site_name" content="Krindex" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://krindex.net" />
+
     <meta name="theme-color" content="#18181b" />
   </Helmet>
 );
