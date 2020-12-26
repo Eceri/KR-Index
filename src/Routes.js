a5a7d6ffd9370d1f807a34e02247caca7d9ee784
LLath <rothpatrick12@yahoo.de>
2020-12-26 17:13:05 +0100

    chore: comment out gears

--- src/Routes.js
+++ src/Routes.js
@@ -3,15 +3,8 @@ import { Switch, Route } from "react-router-dom";
 
 // Relative imports
 import { Notfound } from "./Notfound";
-import {
-  Artifacts,
-  Hero,
-  Heroes,
-  Guides,
-  StatCaps,
-  Gears
-} from "Components";
-import { Home } from "./home"
+import { Artifacts, Hero, Heroes, Guides, StatCaps, Gears } from "Components";
+import { Home } from "./home";
 import { PerksContainer } from "Containers";
 
 export const Routes = () => (
@@ -23,7 +16,8 @@ export const Routes = () => (
     <Route path="/artifacts" component={Artifacts} />
     <Route path="/caps" component={StatCaps} />
     <Route exact path="/guides" component={Guides} />
-    <Route path="/gear" component={Gears} />
+    {/* TODO: Make gears work */}
+    {/* <Route path="/gear" component={Gears} /> */}
     <Route path="/404" component={Notfound} />
     <Route exact path="/" component={Home} />
     <Route path="*" component={Notfound} />
